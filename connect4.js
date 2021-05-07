dbc2e935a2dd9f41fc5a0bdf79d18817c0dcc8ac
Zach Boudreaux <zach-boudreaux@zachBoudreaux.localdomain>
2021-05-07 02:23:18 -0400

    added connect4

--- connect4.js
+++ connect4.js
@@ -5,11 +5,14 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+let WIDTH = 7;
+let HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+let htmlPlayer = document.querySelector('h3'); //says whos turn it is
+htmlPlayer.innerText = 'Turn: Player 1';
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,29 +20,37 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let y = 0; y < HEIGHT; y++){
+    let columnArray = [];
+    for(let x = 0;  x < WIDTH; x++){
+      columnArray.push(undefined);
+    }
+    board.push(columnArray);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  let htmlBoard = document.querySelector('#board');
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // this makes the "top" of the board, the clickable part
+  let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  for (let x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  // this makes the body of the board where the pieces go
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -52,47 +63,63 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for(let y = HEIGHT - 1; y >= 0; y--){
+    if(board[y][x] === undefined){
+      return y;
+    }
+  } 
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
-}
+  const playerPiece = document.createElement('div');
+  const placement = document.getElementById(`${y}-${x}`);
 
-/** endGame: announce game end */
-
-function endGame(msg) {
-  // TODO: pop up alert message
+  playerPiece.classList.add('piece');
+  playerPiece.classList.add(`p${currPlayer}`);
+  playerPiece.classList.add('fall');
+  placement.append(playerPiece);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
   if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+    setTimeout(function(){alert(`Player ${currPlayer} won!`)}, 500);
   }
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  if(board.every((row) => row.every((value) => value !== undefined))){
+      setTimeout(function(){alert(`It's a tie!`)}, 500);
+    }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  if(currPlayer === 1){
+    currPlayer = 2;
+    htmlPlayer.innerText = 'Turn: Player 2';
+  } else {
+    currPlayer = 1
+    htmlPlayer.innerText = 'Turn: Player 1';
+  }
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
